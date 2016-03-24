40d03f89d03387238280cbb947fecf2eb88bf908
Simon <sikking23@yahoo.com>
2016-03-24 12:47:06 +0200

    Added swipe control

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,53 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+  .controller('DashCtrl', function ($scope) {
+  })
+  .controller('DashboardController', function ($scope) {
+    $scope.dashboard = {swiper: false, slider: false, activeIndexView: 2};
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    $scope.$watch('dashboard.slider', function (swiper) {
+      if (swiper) {
+        $scope.swiper = swiper;
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+        swiper.on('onSlideChangeStart', function (swiper) {
+          if(!$scope.$$phase) {
+            $scope.$apply(function () {
+              $scope.dashboard.activeIndexView = swiper.snapIndex;
+              //$scope.enterState()
+              console.log("slide " +swiper.snapIndex);
+            });
+          } else {
+            $scope.dashboard.activeIndexView = swiper.snapIndex;
+          }
+        });
+      }
+    });
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+    $scope.dashboard.slideTo = function (indexSlide) {
+      $scope.swiper.slideTo(indexSlide);
+    };
+  })
+  .controller('ChatsCtrl', function ($scope, Chats, $ionicTabsDelegate) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+      Chats.remove(chat);
+    };
+  })
+
+  .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+  })
+
+  .controller('AccountCtrl', function ($scope) {
+    $scope.settings = {
+      enableFriends: true
+    };
+  });
